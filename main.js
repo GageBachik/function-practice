9ef38d05cd17a5f6674fe8d600890eb1c34f8ac9
gbachik <gbachik@gmail.com>
2014-06-23 17:19:32 -0600

    one third example

---
+++ main.js
@@ -0,0 +1,15 @@
+function tripleFive(){
+	for(var i = 0; i < 3; i++) {
+		console.log('Five!');
+	}
+}
+
+// tripleFive();
+
+function lastLetter(string){
+	console.log(string.slice(string.length-1));
+}
+
+// lastLetter('hello');
+// lastLetter('island');
+
