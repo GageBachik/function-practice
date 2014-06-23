10eb5af3d46000ad88de358b99a46d39d299f72a
gbachik <gbachik@gmail.com>
2014-06-23 17:28:11 -0600

    on indexOf() challenge

--- main.js
+++ main.js
@@ -13,3 +13,38 @@ function lastLetter(string){
 // lastLetter('hello');
 // lastLetter('island');
 
+function negate(number){
+	console.log(-number);
+}
+
+// negate(5);
+// negate(-8);
+
+function toArray(item, item2, item3){
+	var merged = [];
+	merged.push(item);
+	merged.push(item2);
+	merged.push(item3);
+	console.log(merged);
+}
+
+// toArray(1, 4, 5);
+// toArray(8, 9, 10);
+
+function startsWithA(word){
+	if(word[0].toLowerCase() === 'a'){
+		console.log('TRUE');
+	}else {
+		console.log('FALSE');
+	}
+}
+
+// startsWithA('aardvark');
+// startsWithA('bear');
+
+function excite(string){
+	console.log(string + '!!!');
+}
+
+// excite('yes');
+// excite('go');
